2891a4b889c6bc90f4e88dafaa7e27a60f762380
Your Name <manzi5000@hotmail.co.uk>
2024-07-27 12:42:50 +0100

    birthday only

--- emails/birthday-reminder.tsx
+++ emails/birthday-reminder.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Container,
   Head,
+  Heading,
   Hr,
   Html,
   Img,
@@ -20,55 +21,51 @@ const baseUrl = process.env.VERCEL_URL
 export const BirthdayReminderEmail = () => (
   <Html>
     <Head />
-    <Preview>Don't forget it's XYZ's Birthday in XXX days! </Preview>
+
+    <Preview>Don't forget it's Manzi's Birthday in 360 days! </Preview>
     <Body style={main}>
       <Container style={container}>
         <Section style={box}>
           <Img
-            src={`${baseUrl}/static/icons8-cake-64.png`}
+            src={`https://gcdnb.pbrd.co/images/AOg2axderXPh.png?o=1`}
             width="64"
             height="64"
             alt="cake"
           />
+          <Heading style={h1}>You have a birthday coming up!</Heading>
           <Hr style={hr} />
-          <Text style={paragraph}>Manzi's Birthday is in X days.</Text>
+          <Text style={paragraph}>Manzi's Birthday is in 360 days!</Text>
           <Text style={paragraph}>
             Here are some gift ideas they might be interested in:
           </Text>
           <Button style={button} href="#">
             Gift Ideas
           </Button>
-          {/* <Hr style={hr} />
+          <Hr style={hr} />
           <Text style={paragraph}>
-            If you haven't finished your integration, you might find our{" "}
-            <Link style={anchor} href="https://stripe.com/docs">
-              docs
-            </Link>{" "}
-            handy.
+            If you need to make changes to this reminder, you can do so{" "}
+            <Link style={anchor} href="#">
+              here
+            </Link>
+            .
           </Text>
-          <Text style={paragraph}>
+          {/* <Text style={paragraph}>
             Once you're ready to start accepting payments, you'll just need to
             use your live{" "}
-            <Link
-              style={anchor}
-              href="https://dashboard.stripe.com/login?redirect=%2Fapikeys"
-            >
+            <Link style={anchor} href="#">
               API keys
             </Link>{" "}
             instead of your test API keys. Your account can simultaneously be
             used for both test and live requests, so you can continue testing
             while accepting live payments. Check out our{" "}
-            <Link style={anchor} href="https://stripe.com/docs/dashboard">
+            <Link style={anchor} href="#">
               tutorial about account basics
             </Link>
             .
           </Text>
           <Text style={paragraph}>
             Finally, we've put together a{" "}
-            <Link
-              style={anchor}
-              href="https://stripe.com/docs/checklist/website"
-            >
+            <Link style={anchor} href="#">
               quick checklist
             </Link>{" "}
             to ensure your website conforms to card network standards.
@@ -76,16 +73,14 @@ export const BirthdayReminderEmail = () => (
           <Text style={paragraph}>
             We'll be here to help you with any step along the way. You can find
             answers to most questions and get in touch with us on our{" "}
-            <Link style={anchor} href="https://support.stripe.com/">
+            <Link style={anchor} href="#">
               support site
             </Link>
             .
-          </Text>
-          <Text style={paragraph}>— The Stripe team</Text>
-          <Hr style={hr} />
-          <Text style={footer}>
-            Stripe, 354 Oyster Point Blvd, South San Francisco, CA 94080
           </Text> */}
+          <Text style={paragraph}>— #TeamSZMM</Text>
+          <Hr style={hr} />
+          <Text style={footer}>Project Name Here?</Text>
         </Section>
       </Container>
     </Body>
@@ -98,6 +93,16 @@ const main = {
   backgroundColor: "#f6f9fc",
   fontFamily:
     '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
+};
+
+const h1 = {
+  color: "#525f7f",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "24px",
+  fontWeight: "bold",
+  margin: "40px 0",
+  padding: "0",
 };
 
 const container = {
